faac69c60f325a6dbe462a3dc43a4fb701ea8462
hohoaviethong <hohoaviethong247@gmail.com>
2025-07-08 20:41:41 +0700

    newww

--- pages/admin_page.py
+++ pages/admin_page.py
@@ -1,43 +1,41 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait   
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-import pytest
-from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait   
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.common.keys import Keys
+# import pytest
+# from selenium.webdriver.support import expected_conditions as EC
+# from time import sleep
 
-class AdminPage:
-    def __init__(self, driver):
-        self.driver = driver 
-        self.admin_btn = (By.XPATH, "//a[@class='oxd-main-menu-item active']")
-        self.title = (By.CSS_SELECTOR, ".oxd-topbar-header-title")
+# class AdminPage:
+#     def __init__(self, driver):
+#         self.driver = driver 
+#         self.admin_btn = (By.XPATH, "//a[@class='oxd-main-menu-item active']")
+#         self.title = (By.CSS_SELECTOR, ".oxd-topbar-header-title")
 
-    # def login_to_admin(self, username: str, password: str):
-    #     self.login_page.login(username, password)
 
-    # def is_admin_displayed(self):
-    #     try:
-    #        sleep(5)
-    #        element = WebDriverWait(self.driver, 10).until(
-    #            EC.visibility_of_element_located(*self.admin_btn))
-    #        return True 
-    #     except:
-    #         return False
+#     # def is_admin_displayed(self):
+#     #     try:
+#     #        sleep(5)
+#     #        element = WebDriverWait(self.driver, 10).until(
+#     #            EC.visibility_of_element_located(*self.admin_btn))
+#     #        return True 
+#     #     except:
+#     #         return False
 
-    def is_admin_clickable(self):
-        try:
-            sleep(5)
-            admin = self.driver.find_element(*self.admin_btn)
-            return admin.is_enabled()
-        except:
-            return False
+#     def is_admin_clickable(self):
+#         try:
+#             sleep(5)
+#             admin = self.driver.find_element(*self.admin_btn)
+#             return admin.is_enabled()
+#         except:
+#             return False
         
-    def is_title_displayed(self):
-        try:
-            return self.driver.find_element(*self.title).is_displayed()
-        except:
-            return False
+#     def is_title_displayed(self):
+#         try:
+#             return self.driver.find_element(*self.title).is_displayed()
+#         except:
+#             return False
     
 
 
--- tests/admin_test.py
+++ tests/admin_test.py
@@ -1,22 +1,22 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait   
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-import pytest
-from pages.login_page import LoginPage
-from tests.base_test import BaseTest
-from pages.admin_page import AdminPage
-@pytest.mark.ui
-@pytest.mark.skip
-class TestAdmin(BaseTest):  
-    def test_admin_page(self):
-        login_page = LoginPage(self.driver)
-        login_page.login("Admin", "admin123")
-        admin_page = AdminPage(self.driver)
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait   
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.common.keys import Keys
+# import pytest
+# from pages.login_page import LoginPage
+# from tests.base_test import BaseTest
+# from pages.admin_page import AdminPage
+# @pytest.mark.ui
+# @pytest.mark.skip
+# class TestAdmin(BaseTest):  
+#     def test_admin_page(self):
+#         login_page = LoginPage(self.driver)
+#         login_page.login("Admin", "admin123")
+#         admin_page = AdminPage(self.driver)
 
-        self.driver.implicitly_wait(15)
+#         self.driver.implicitly_wait(15)
       
-        # assert admin_page.is_admin_displayed()
-        assert admin_page.is_admin_clickable()
-        assert admin_page.is_title_displayed()
+#         # assert admin_page.is_admin_displayed()
+#         assert admin_page.is_admin_clickable()
+#         assert admin_page.is_title_displayed()
